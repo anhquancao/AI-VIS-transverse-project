5394a2fb1ae2ccfa9f7f098792f603176f3773a2
caoquan95 <aquancva@gmail.com>
2020-03-18 21:18:01 +0000

    add face segmentation

--- pytorch-AdaIN/net.py
+++ pytorch-AdaIN/net.py
@@ -4,6 +4,7 @@ from function import adaptive_instance_normalization as adain
 from function import calc_mean_std
 from lib.loss_depth import L2Loss, GradientLoss, NormalLoss, BerHu
 from lib.depth_net import DepthV3
+from lib.bisenet import BiSeNet
 import torch
 
 decoder = nn.Sequential(
@@ -177,3 +178,83 @@ class Net(nn.Module):
         for i in range(1, 4):
             loss_s += self.calc_style_loss(g_t_feats[i], style_feats[i])
         return loss_c, loss_s, loss_d, stylized_depth, content_depth
+    
+    
+class NetFaceSegment(nn.Module):
+    def __init__(self, encoder, decoder, face_net):
+        super(NetFaceSegment, self).__init__()
+        enc_layers = list(encoder.children())
+        n_classes = 19
+        self.face_net = face_net
+        self.enc_1 = nn.Sequential(*enc_layers[:4])  # input -> relu1_1
+        self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
+        self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
+        self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
+        self.decoder = decoder
+        self.mse_loss = nn.MSELoss()
+        for param in self.face_net.parameters():
+            param.requires_grad = False
+        self.face_net.eval()
+    
+        # fix the encoder
+        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4']:
+            for param in getattr(self, name).parameters():
+                param.requires_grad = False
+
+    # extract relu1_1, relu2_1, relu3_1, relu4_1 from input image
+    def encode_with_intermediate(self, input):
+        results = [input]
+        for i in range(4):
+            func = getattr(self, 'enc_{:d}'.format(i + 1))
+            results.append(func(results[-1]))
+        return results[1:]
+
+    # extract relu4_1 from input image
+    def encode(self, input):
+        for i in range(4):
+            input = getattr(self, 'enc_{:d}'.format(i + 1))(input)
+        return input
+
+    def calc_content_loss(self, input, target):
+        assert (input.size() == target.size())
+        assert (target.requires_grad is False)
+        return self.mse_loss(input, target)
+
+    def calc_style_loss(self, input, target):
+        assert (input.size() == target.size())
+        assert (target.requires_grad is False)
+        input_mean, input_std = calc_mean_std(input)
+        target_mean, target_std = calc_mean_std(target)
+        return self.mse_loss(input_mean, target_mean) + \
+               self.mse_loss(input_std, target_std)
+    
+    def calc_face_segmentation_loss(self, input, target):
+        assert (input.size() == target.size())
+        assert (target.requires_grad is False)
+        lossL2 = L2Loss()
+        return lossL2(input, target)
+        
+
+    def forward(self, content, style, alpha=1.0):
+        assert 0 <= alpha <= 1
+        style_feats = self.encode_with_intermediate(style)
+        content_feat = self.encode(content)
+        content_face_segmented = self.face_net(content)[0]
+        
+        t = adain(content_feat, style_feats[-1])
+        t = alpha * t + (1 - alpha) * content_feat
+
+        g_t = self.decoder(t)
+        g_t_feats = self.encode_with_intermediate(g_t)
+        stylized_face_segmented = self.face_net(g_t)[0]
+ 
+        
+
+        loss_c = self.calc_content_loss(g_t_feats[-1], t)
+        loss_s = self.calc_style_loss(g_t_feats[0], style_feats[0])
+        loss_f = self.calc_face_segmentation_loss(stylized_face_segmented, content_face_segmented)
+            
+        for i in range(1, 4):
+            loss_s += self.calc_style_loss(g_t_feats[i], style_feats[i])
+        return loss_c, loss_s, loss_f, stylized_face_segmented, content_face_segmented
+
